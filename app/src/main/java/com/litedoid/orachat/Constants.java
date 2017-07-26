2fe0fdbd8bd05b032c9229d1563d8723049a4d78
Andrew Lux <litedroiddevelopment@gmail.com>
2017-07-26 09:08:55 -0700

    Add Resources
    project structure

---
+++ app/src/main/java/com/litedoid/orachat/Constants.java
@@ -0,0 +1,8 @@
+package com.litedoid.orachat;
+
+public class Constants
+{
+    public final static String API_ENDPOINT = "https://private-93240c-oracodechallenge.apiary-mock.com/";
+
+
+}
--- app/src/main/java/com/litedoid/orachat/activity/MainActivity.java
+++ app/src/main/java/com/litedoid/orachat/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.litedoid.orachat;
+package com.litedoid.orachat.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.litedoid.orachat.R;
 
 public class MainActivity extends AppCompatActivity
 {
