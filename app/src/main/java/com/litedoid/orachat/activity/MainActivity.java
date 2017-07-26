de134247cb60d195e52fdde4ba8acd66a7f6a5c4
Andrew Lux <litedroiddevelopment@gmail.com>
2017-07-26 10:10:27 -0700

    Convert to annotations

--- app/src/main/java/com/litedoid/orachat/activity/MainActivity.java
+++ app/src/main/java/com/litedoid/orachat/activity/MainActivity.java
@@ -1,17 +1,28 @@
 package com.litedoid.orachat.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.litedoid.orachat.R;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity
 {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    }
+
+    @AfterViews
+    void initViews()
+    {
+        Log.d(TAG, "initViews");
     }
 }
